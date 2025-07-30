5eeaf9a6210f63628758d15233e4a56fd093043f
alexsoulless <onilove.sa@gmail.com>
2025-07-30 13:28:57 +0300

    add responce model

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -3,10 +3,11 @@ from app.db import get_session
 from sqlalchemy.future import select
 from app.models import User
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.schemas.user import UserSchema
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get("")
+@router.get("", response_model=list[UserSchema])
 async def get_users(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(User))
     users = result.scalars().all()
