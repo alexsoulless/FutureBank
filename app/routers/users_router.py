f9221cd6edd279d3a5dd572c20aba157c443bcf8
alexsoulless <onilove.sa@gmail.com>
2025-07-30 18:34:50 +0300

    write get user

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -7,8 +7,20 @@ from app.schemas.user import UserSchema
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+
 @router.get("", response_model=list[UserSchema])
 async def get_users(session: AsyncSession = Depends(get_session)):
     result = await session.execute(select(User))
     users = result.scalars().all()
     return users
+
+
+@router.get("/{user_id}", response_model=UserSchema)
+async def get_user(
+    session: AsyncSession = Depends(get_session), user_id: int = Path(..., ge=1)
+):
+    result = await session.execute(select(User).filter_by(id=user_id))
+    user = result.scalar_one_or_none()
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return UserSchema(**user)
