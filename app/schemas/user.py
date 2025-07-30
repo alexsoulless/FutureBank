1998f31b24391971046820b1e4384554b1e5cadf
alexsoulless <onilove.sa@gmail.com>
2025-07-30 18:43:39 +0300

    fill models

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -10,9 +10,20 @@ class UserSchema(BaseModel):
     balance: Decimal
     is_banned: bool
     role_id: int
-    
-class UserCreateSchema(UserSchema):
-    pass
 
-class UserUpdateSchema(UserSchema):
-    pass
+    class Config:
+        orm_mode = True
+    
+class UserCreateSchema(BaseModel):
+    username: str
+    fio: str
+    balance: Optional[Decimal] = Decimal(0)
+    is_banned: Optional[bool] = False
+    role_id: int
+
+class UserUpdateSchema(BaseModel):
+    username: Optional[str] = None
+    fio: Optional[str] = None
+    balance: Optional[Decimal] = None
+    is_banned: Optional[bool] = None
+    role_id: Optional[int] = None
