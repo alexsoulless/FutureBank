fbc66c2bd4334570aee225d34e4c6bacc0cc4e44
alexsoulless <onilove.sa@gmail.com>
2025-07-31 18:45:29 +0300

    imlp get taxes

--- app/routers/taxes_router.py
+++ app/routers/taxes_router.py
@@ -1,3 +1,17 @@
-from fastapi import APIRouter, HTTPException, Query, Path, status
+from fastapi import APIRouter, HTTPException, Query, Path, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from app.schemas.tax import TaxSchema, TaxCreateSchema, TaxUpdateSchema
+from app.models import Tax
+from app.db import get_session
 
 router = APIRouter(prefix="/taxes", tags=["taxes"])
+
+@router.get("", response_model=list[TaxSchema])
+async def get_taxes(session: AsyncSession = Depends(get_session)):
+    try:
+        result = await session.execute(select(Tax))
+        taxes = result.scalars().all()
+        return taxes
+    finally:
+        await session.close()
