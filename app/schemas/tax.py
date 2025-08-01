1b7f65f3f1415f4af8ced1b020fd7301baa837c1
alexsoulless <onilove.sa@gmail.com>
2025-08-01 18:49:39 +0300

    again

--- app/schemas/tax.py
+++ app/schemas/tax.py
@@ -2,24 +2,22 @@ from typing import Optional
 from pydantic import BaseModel, Field
 from datetime import datetime
 from decimal import Decimal
-from app.schemas.user import UserSchema
+
 
 class TaxSchema(BaseModel):
     id: int
     name: str
     due_datetime: datetime
     amount: Decimal
-    
+
+
 class TaxCreateSchema(BaseModel):
     name: str
     due_datetime: datetime
     amount: Optional[Decimal] = Decimal("0.00")
 
+
 class TaxUpdateSchema(BaseModel):
     name: Optional[str] = None
     due_datetime: Optional[datetime] = None
     amount: Optional[Decimal] = None
-
-class TaxStatsSchema(BaseModel):
-    payed: list[UserSchema]
-    not_payed:list[UserSchema]
